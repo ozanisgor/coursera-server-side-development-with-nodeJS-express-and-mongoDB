1fc216a244f729e27e45e402c572e973cd8e3141
ozan isgor <ozanisgor11@gmail.com>
2022-11-08 19:02:39 +0300

    REST API for the promotions

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const http = require('http');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -14,6 +15,7 @@ app.use(morgan('dev'));
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
 
 app.use((req, res, next) => {
   console.log(req.headers);
--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const dishRouter = express.Router();
 
 dishRouter.use(bodyParser.json());
